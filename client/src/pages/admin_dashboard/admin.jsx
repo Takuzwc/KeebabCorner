c0b27153bbad9ba76adcb2e48aec1a2b169233b0
Takuzwc <takungwenya42@outlook.com>
2024-08-13 22:20:29 +0200

    added admin dashboard

---
+++ client/src/pages/admin_dashboard/admin.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Fragment } from 'react';
+
+export class Admin extends Component {
+  render() {
+    return (
+      <div className="mt-5 d-flex justify-content-left">
+        <h1>Admin Dashboard</h1>
+      </div>
+    );
+  }
+}
