0e3bc6e27acd38891b5db6036d63ae954f8742ee
Takuzwc <takungwenya42@outlook.com>
2024-11-06 14:23:23 +0100

    another bug in about page

--- client/src/pages/user_dashboard/about_page.jsx
+++ client/src/pages/user_dashboard/about_page.jsx
@@ -3,7 +3,7 @@ import './AboutPage.css';
 import { Order_main_component } from '../../../public/components/main_components/Ordering_main_component/ordering_main_component';
 import { Nav_main_component } from '../../../public/components/main_components/Nav_main_component/nav_main_component';
 import { Footer_main_component } from '../../../public/components/main_components/Footer_main_component/footer_main_component';
-import { ContactForm } from '/Users/Taku/Documents/git_Workspace/KeebabCorner/client/public/components/componentList/contactForm/contactForm.jsx';
+import { ContactForm } from '/../client/public/components/componentList/contactForm/contactForm.jsx';
 import { aboutText } from '../../../public/components/componentList/box_menu_comp/box_menu_data';
 
 export function AboutText() {
