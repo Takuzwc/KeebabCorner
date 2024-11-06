75b594d9638ef3d7a8f76a541b1bed7365edf12a
Takuzwc <takungwenya42@outlook.com>
2024-11-06 11:52:35 +0100

    made a few changes

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
-//PostgreSQL connection setup
 const pool = new Pool({
   user: 'Taku',
   host: 'localhost',
@@ -10,11 +9,9 @@ const pool = new Pool({
   password: process.env.POSTGRESS_PWD,
   port: 5432,
 });
-
 // Endpoint to save the payment details
 exports.createPayment = async (req, res) => {
   const { orders, totalCost } = req.body;
-
   try {
     const result = await pool.query(
       'INSERT INTO payments (order_details, total_cost) VALUES ($1, $2) RETURNING *',
--- server/server.js
+++ server/server.js
@@ -5,10 +5,22 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 //Connection to MongoDB server
-mongoose.connect('mongodb://localhost:27017/keebabDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect('mongodb://localhost:27017/keebabDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'))
+  .catch((error) => console.error('DB connection error:', error));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
